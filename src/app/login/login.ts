f94b73a58c8d7479d45d72ef68f6ef97e0efcfee
AraceliAG <131417012+AraceliAG@users.noreply.github.com>
2025-08-23 16:27:52 -0600

    Login desingn was applied

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   imports: [],
   templateUrl: './login.html',
-  styleUrl: './login.css'
+  styleUrl: './login.css',
+  encapsulation: ViewEncapsulation.None // ⚠ Desactiva el encapsulamiento
 })
 export class Login {
+
+  onSubmit(){}
 
 }
