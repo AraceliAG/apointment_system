913d690b0baeef36cf9849cd96c0006b9e62aa41
AraceliAG <131417012+AraceliAG@users.noreply.github.com>
2025-08-22 21:57:22 -0600

    model and services were creted and also user, appointment and doctor file  were created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Users } from './users/users';
+import { Login } from './login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component:Login},
+    {path:'users', component:Users}
+];
