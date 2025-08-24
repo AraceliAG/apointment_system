1ece231e650f0673197472632f683cf4a908e67b
AraceliAG <131417012+AraceliAG@users.noreply.github.com>
2025-08-24 15:16:39 -0600

    Environment file was created and also modelUser created

---
+++ src/app/models/user.ts
@@ -0,0 +1,10 @@
+export class User{
+    number_SS:string = "";
+    name:string="";
+    last_Name:string="";
+    phone:string="";
+    number_Appointment="";
+    curp:string="";
+    name_Hospital:string="";
+
+}
---
+++ src/environment.ts
@@ -0,0 +1,9 @@
+class Environment {
+    API:string = "https://localhost:7248/"; // Desarrollo
+    //public API: string = "http://glas845a:8083/api/"; //PROD
+   
+}
+ 
+let Env:Environment = new Environment();
+ 
+export default Env;
