1b68ed1b19cdb738a6cd864f43118fc1fcdbdf37
AraceliAG <131417012+AraceliAG@users.noreply.github.com>
2025-08-25 21:04:05 -0600

    users view styles were added

--- src/app/users/users.ts
+++ src/app/users/users.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation  } from '@angular/core';
 
 @Component({
   selector: 'app-users',
   imports: [],
   templateUrl: './users.html',
-  styleUrl: './users.css'
+  styleUrl: './users.css',
+  encapsulation:ViewEncapsulation.None
 })
 export class Users {
 
